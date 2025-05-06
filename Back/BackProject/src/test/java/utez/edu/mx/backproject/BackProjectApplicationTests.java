cd422718e0c9f4ed8972bad869f9d21a2990a94d
Alan Daniel <20223tn018@utez.edu.mx>
2025-05-06 15:50:24 -0600

    Project Upload

---
+++ Back/BackProject/src/test/java/utez/edu/mx/backproject/BackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.backproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
